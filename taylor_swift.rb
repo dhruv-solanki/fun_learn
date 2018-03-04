579af045581ff504b51a15e4b33be1ec1dd37b69
dhruv911 <dhruvsolanki34@gmail.com>
2018-03-04 15:33:34 +0530

    Create taylor_swift.rb

---
+++ taylor_swift.rb
@@ -0,0 +1,12 @@
+puts "Do you like Taylor Swift (enter Yes or No)"
+
+value = gets.chomp
+
+if value != 'Yes'
+  puts "And the haters gonna hate, hate, hate, hate, hate"
+  puts "Baby, I'm just gonna shake, shake, shake, shake, shake"
+  puts "I shake it off, I shake it off"
+end
+
+
+puts "Uh oh!  I don't know about you! But I'm feeling 22"
